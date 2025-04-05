693f540c1c90da9a17131427ae77bdf7cf429ca3
Gabriela Alva <alvagabriela272@gmail.com>
2025-04-05 17:51:22 -0600

    Modificacion de entity user

--- TecPurisima.School.Core/Dto/UserDto.cs
+++ TecPurisima.School.Core/Dto/UserDto.cs
@@ -6,7 +6,6 @@ public class UserDto: DtoBase
     public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
-    public int StudentId { get; set; }
     
     public UserDto()
     {
@@ -19,6 +18,5 @@ public class UserDto: DtoBase
         Username = user.Username;
         Password = user.Password;
         Email = user.Email;
-        StudentId = user.StudentId;
     }
 }
--- TecPurisima.School.Core/Entities/User.cs
+++ TecPurisima.School.Core/Entities/User.cs
@@ -5,6 +5,5 @@ public class User: EntityBase
     public string Username { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
-    public int StudentId { get; set; }
     
 }
